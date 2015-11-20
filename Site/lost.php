d4bb7667b6f3f6d6a2275012e44a05d226c7a026
Scott123180 <Scott123180@gmail.com>
2015-11-20 11:15:45 -0500

    bringing over code from other labs

--- Site/lost.php
+++ Site/lost.php
@@ -28,6 +28,91 @@
             <div class="col-md-4" style="background-color:#cc0000; text-align:center"><a href="about.php"><h4>About</h4></a></div>
         </div>
     </div>
+
+    </div class="container">
+
+        <?php
+        # Connect to MySQL server and the database
+        require( 'php_includes/connect_db.php' ) ;
+
+        # Includes these helper functions
+        require( 'php_includes/helpers.php' ) ;
+
+        # Check form submitted.
+
+        if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' ){
+
+        # Initialize an error array.
+
+            $errors = array();
+
+
+        # Check for a number & first name & last name.
+
+        #num
+            if ( empty( $_POST[ 'num' ]) )   {
+                $errors[] = 'num' ; }
+
+            else { $num = trim( $_POST[ 'num' ] )  ; }
+
+        #first name
+            if ( empty( $_POST[ 'fname' ]) )   {
+                $errors[] = 'fname' ; }
+
+            else { $fname = trim( $_POST[ 'fname' ] )  ; }
+
+        #last name
+            if ( empty( $_POST[ 'lname' ] ) )  {
+                $errors[] = 'lname' ; }
+
+            else { $lname = trim( $_POST[ 'lname' ] )  ; }
+
+
+        # Report result.
+
+            if( !empty( $errors ) )
+            {
+
+                echo 'Error! Please enter your  ' ;
+
+                foreach ( $errors as $msg ) { echo " - $msg " ; }
+            }
+
+            else {
+                $result = insert_record($dbc, $num, $fname, $lname) ;
+                echo "Success! Thanks $fname " ; }
+        }
+
+        #get link num and show specific record
+        else if($_SERVER[ 'REQUEST_METHOD' ] == 'GET') {
+            if(isset($_GET['num']))
+                show_record($dbc, $_GET['num']) ;
+        }
+
+        # Show the records
+        show_link_records($dbc);
+
+        # Close the connection
+        mysqli_close( $dbc ) ;
+        ?>
+    <!-- Get inputs from the user. -->
+    <form action="linkypresidents.php" method="POST">
+        <table>
+            <tr>
+                <td>President Number:</td><td><input type="text" name="num" value="<?php if (isset($_POST['num'])) echo $_POST['num']; ?>"></td>
+            </tr>
+            <tr>
+                <td>First Name:</td><td><input type="text" name="fname" value="<?php if (isset($_POST['fname'])) echo $_POST['fname']; ?>"></td>
+            </tr>
+            <tr>
+                <td>Last Name:</td><td><input type="text" name="lname" value="<?php if (isset($_POST['lname'])) echo $_POST['lname']; ?>"></td>
+            </tr>
+        </table>
+        <p><input type="submit" ></p>
+    </form>
+
+
+    </div>
 	<div class="container-fluid">
 
         <div class="row">
---
+++ Site/php_includes/helpers.php
@@ -0,0 +1,187 @@
+<!--
+=========================================================
+Unable to solve during office hours, problem is on line 77 linkypresidents and SQL query
+=========================================================
+-->
+
+<?php
+$debug = true;
+
+#Scott Hansen and Nicholas Burd
+
+# Shows the records in prints
+function show_records($dbc) {
+	# Create a query to get the name and price sorted by price
+	$query = 'SELECT * FROM presidents ;' ;
+
+	# Execute the query
+	$results = mysqli_query( $dbc , $query ) ;
+	check_results($results) ;
+
+	# Show results
+	if( $results )
+	{
+  		# But...wait until we know the query succeed before
+  		# rendering the table start.
+  		echo '<H1>Presidents</H1>' ;
+  		echo '<TABLE>';
+  		echo '<TR>';
+  		echo '<TH>Number</TH>';
+  		echo '<TH>First Name</TH>';
+  		echo '<TH>Last Name</TH>';
+  		echo '</TR>';
+
+  		# For each row result, generate a table row
+  		while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+  		{
+    		echo '<TR>' ;
+    		echo '<TD>' . $row['num'] . '</TD>' ;
+    		echo '<TD>' . $row['fname'] . '</TD>' ;
+    		echo '<TD>' . $row['lname'] . '</TD>' ;
+    		echo '</TR>' ;
+  		}
+
+  		# End the table
+  		echo '</TABLE>';
+
+  		# Free up the results in memory
+  		mysqli_free_result( $results ) ;
+	}
+}
+
+function show_link_records($dbc) {
+	# Create a query to get the name and price sorted by price
+	$query = 'SELECT num, lname FROM presidents ;' ;
+
+	# Execute the query
+	$results = mysqli_query( $dbc , $query ) ;
+	check_results($results) ;
+
+	# Show results
+	if( $results )
+	{
+  		# But...wait until we know the query succeed before
+  		# rendering the table start.
+  		echo '<H1>Presidents</H1>' ;
+  		echo '<TABLE>';
+  		echo '<TR>';
+  		echo '<TH>Number</TH>';
+  		echo '<TH>Last Name</TH>';
+  		echo '</TR>';
+
+  		# For each row result, generate a table row
+  		while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+  		{
+			$alink = '<A HREF=linkypresidents.php?num=' . $row['num'] . '>' . $row['num'] . '</A>' ;
+    		echo '<TR>' ;
+    		echo '<TD ALIGN=right>'. $alink . '</TD>' ;
+    		echo '<TD>' . $row['lname'] . '</TD>' ;
+    		echo '</TR>' ;
+  		}
+		
+
+  		# End the table
+  		echo '</TABLE>';
+
+  		# Free up the results in memory
+  		mysqli_free_result( $results ) ;
+	}
+}
+
+function show_record($dbc, $num) {
+	# Create a query to get the name and price sorted by price
+	
+	
+	$query = 'SELECT num, fname, lname FROM presidents WHERE num=' . $num . ';' ;
+
+
+	# Execute the query
+	$results = mysqli_query( $dbc , $query ) ;
+	check_results($results) ;
+
+	# Show results
+	if( $results )
+	{
+  		# But...wait until we know the query succeed before
+  		# rendering the table start.
+  		echo '<H1>Presidents</H1>' ;
+  		echo '<TABLE>';
+  		echo '<TR>';
+  		echo '<TH>Number</TH>';
+  		echo '<TH>First Name</TH>';
+  		echo '<TH>Last Name</TH>';
+  		echo '</TR>';
+
+  		# For each row result, generate a table row
+  		while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+  		{
+    		echo '<TR>' ;
+    		echo '<TD>' . $row['num'] . '</TD>' ;
+    		echo '<TD>' . $row['fname'] . '</TD>' ;
+    		echo '<TD>' . $row['lname'] . '</TD>' ;
+    		echo '</TR>' ;
+  		}
+
+  		# End the table
+  		echo '</TABLE>';
+
+  		# Free up the results in memory
+  		mysqli_free_result( $results ) ;
+	}
+}
+
+# Inserts a record into the prints table
+function insert_record($dbc, $num, $fname,$lname) {
+  $query = 'INSERT INTO presidents(num, fname,lname) VALUES ("' . $num . '" , "' . $fname . '" , "' . $lname . '" )' ;
+  show_query($query);
+
+  $results = mysqli_query($dbc,$query) ;
+  check_results($results) ;
+
+  return $results ;
+}
+
+function valid_number($num) {
+ if(empty($num) || !is_numeric($num))
+ return false ;
+ else {
+ $num = intval($num) ;
+ if($num <= 0)
+ return false ;
+ }
+ return true ;
+}
+
+function valid_name($name) {
+	if (empty($name)) {
+		return false;
+	} else {return true;}
+}
+
+# Shows the query as a debugging aid
+function show_query($query) {
+  global $debug;
+
+  if($debug)
+    echo "<p>Query = $query</p>" ;
+}
+
+# Checks the query results as a debugging aid
+function check_results($results) {
+  global $dbc;
+
+  if($results != true)
+    echo '<p>SQL ERROR = ' . mysqli_error( $dbc ) . '</p>'  ;
+}
+
+#prints a javascript console message for debugging
+function js_console_debug($message) {
+	if ($message != '') {
+		echo '<script type="text/javascript">console.log("' . $message . '")</script>' ;
+		return;
+	} else {
+		echo '<script type="text/javascript">console.log("failed to debug")</script>' ;
+		return;
+	}
+}
+?>
