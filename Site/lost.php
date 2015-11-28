90199d0aeddd9ca095a8441803d112b91b84eb3a
Scott123180 <Scott123180@gmail.com>
2015-11-28 17:33:23 -0500

    adding select options

--- Site/lost.php
+++ Site/lost.php
@@ -30,7 +30,25 @@
 
     <!--select options-->
     <div class="container">
-        
+        <!--item category, how long ago lost, where lost-->
+        <h5>Item Category</h5>
+        <select class="form-control">
+            <option>phone or computer</option>
+            <option>audio or headphones</option>
+            <option>clothing</option>
+            <option>notebook or books</option>
+            <option>bag or backpack</option>
+            <option>other</option>
+        </select>
+        <h5>How long ago was it lost?</h5>
+        <select class="form-control">
+            <option>today</option>
+            <option>yesterday</option>
+            <option>2 to 7 days</option>
+            <option>longer than a week</option>
+        </select>
+        <h5>What building did you lose it in?</h5>
+        <select></select>
     </div>
     <!--tables-->
     <div class="container">
