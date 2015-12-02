e3c28a3fa5006dd8da8544ec2ba574da5d062158
Scott123180 <Scott123180@gmail.com>
2015-12-01 20:24:19 -0500

    starting work again

--- Site/lost.php
+++ Site/lost.php
@@ -73,7 +73,7 @@ if it has been found. If it hasn't, the user can add an entry to the database
         require( 'php_includes/helpers.php' ) ;
 
         #if GET request
-        if($_SERVER[ 'REQUEST_METHOD' ] === '_GET') {
+        if($_SERVER[ 'REQUEST_METHOD' ] == '$_GET') {
             #if GET id in GET request, show the record of that item
             if(isset($_GET['id'])) {
                 show_record($dbc, $_GET['id']) ; }
