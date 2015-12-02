adb528451fe600e77688d77d660c7d4e6e31b0ae
Nicholas Burd <hocliansnb@gmail.com>
2015-12-02 13:23:42 -0500

    started password change page

--- Site/admin_change_password.php
+++ Site/admin_change_password.php
@@ -8,27 +8,27 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
-    <title>Please Login</title>
+    <title>Welcome to Limbo</title>
 </head>
 <body>
     <div class="container-fluid" style="background-color:#aaaaaa">
         <div class="row">
             <div class="span4"></div>
-            <div class="span4"><a href="index.php"><img class="img-responsive center-block" src="images/limbocompressed.png" style="padding-bottom:20px; padding-top:10px" /></a></div>
+            <div class="span4"><a href="index.php"><img class="img-responsive center-block" src="images/LimboCompressed.png" style="padding-bottom:20px; padding-top:10px" /></a></div>
             <div class="span4"></div>
         </div>
     </div>
 
     <div class="container-fluid">
+
         <!--Header-->
         <div class="row">
-            <div class="col-md-4" style="background-color:#cc0000; text-align:center"><a href="lost.php"><h4>Lost Something?</h4></a></div>
-            <div class="col-md-4" style="background-color:#cc0000; text-align:center"><a href="found.php"><h4>Found Something?</h4></a></div>
-            <div class="col-md-4" style="background-color:#cc0000; text-align:center"><a href="about.php"><h4>About</h4></a></div>
+            <div class="col-md-4" style="background-color:#cc0000; text-align:center"><a href="lost.php" style="color:white;"><h4>Lost Something?</h4></a></div>
+            <div class="col-md-4" style="background-color:#cc0000; text-align:center"><a href="found.php" style="color:white;"><h4>Found Something?</h4></a></div>
+            <div class="col-md-4" style="background-color:#cc0000; text-align:center"><a href="about.php" style="color:white;"><h4>About</h4></a></div>
         </div>
     </div>
-
-    <div class="container">
+	<div class="container">
         <h1>Hey Admin! Please login below.</h1>
 		<?php
 			# Connect to MySQL server and the database
