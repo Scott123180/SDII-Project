97e00f317089f6236a540a2d14616032e5b54e86
Scott123180 <Scott123180@gmail.com>
2015-12-03 02:43:25 -0500

    finished basic input for the form

--- Site/create_lost_item.php
+++ Site/create_lost_item.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/helpers.js"></script>
 
     <title>Lost Something</title>
 </head>
@@ -30,28 +31,54 @@
 
     <!--select options-->
     <div class="container">
-        <form id="createLost" method="post" action="create_lost_item.php">
-            <h4>Item Category</h4>
-            <select class="form-control" name="iCat" id="iCat">
-                <option>item category</option>
-                <script>makeOptions(itemCategories, "iCat");</script>
-            </select>
-
-            <h4>How long ago was it lost?</h4>
-            <select class="form-control" name="tLost" id="tLost">
-                <option>time lost</option>
-                <script>makeOptions(timeRanges, "tLost")</script>
-                <option>unknown</option>
-            </select>
-
+        <form class="form-group">
             <h4>What location did you lose it at?</h4>
             <select class="form-control" name="campLoc" id="campLoc">
                 <option>location</option>
                 <script>makeOptions(campusLocations, "campLoc")</script>
                 <option>unknown</option>
             </select>
+            <h4>How long ago was it lost?</h4>
+            <select class="form-control" name="tLost" id="tLost">
+                <option>time lost</option>
+                <script>makeOptions(timeRanges, "tLost")</script>
+                <option>unknown</option>
+            </select>
+            <h4>What is name of the item?</h4>
+            <input type="text" class="form-control" placeholder="Text input">
+
+            <h4>Please describe the item:</h4>
+            <textarea class="form-control" rows="3"></textarea>
+
+            <h4>What room did you lose it in?</h4>
+            <input type="text" class="form-control" placeholder="Text input">
+
+
+            <h4>Item Category</h4>
+            <select class="form-control" name="iCat" id="iCat">
+                <option>item category</option>
+                <script>makeOptions(itemCategories, "iCat");</script>
+            </select>
+
+            <h4>What is the item's make?</h4>
+            <input type="text" class="form-control" placeholder="Text input">
+
+
+            <h4>What is the item's color?</h4>
+            <input type="text" class="form-control" placeholder="Text input">
+
+
+            <h4>Do you wish to offer a reward for the item?</h4>
+            <label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
+            <div class="input-group">
+                <div class="input-group-addon">$</div>
+                <input type="text" class="form-control" id="exampleInputAmount" placeholder="Amount">
+                <div class="input-group-addon">.00</div>
+            </div>
+
 
             <input type="submit" class="form-control" name = 'submitFilter' value="Submit" style="margin-top: 15px" />
+            <br/>
         </form>
         <br/>
     </div>
