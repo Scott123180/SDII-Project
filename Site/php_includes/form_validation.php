f7f82d0dafc0e56e34866d7c9b09040ab8096eed
Scott123180 <Scott123180@gmail.com>
2015-12-04 03:07:43 -0500

    validation pretty much done

--- Site/php_includes/form_validation.php
+++ Site/php_includes/form_validation.php
@@ -45,26 +45,93 @@ function validateCreateLost($location, $room, $dateLost, $name, $description, $c
  */
 #checks to see if location is at Marist
 function validateLocation($location) {
-    return false;
+    $campusLocations = array(
+        'byrne house',
+        'james a. cannavino library',
+        'champagnat hall',
+        'our lady seat of wisdom chapel',
+        'cornell boathouse',
+        'donnelly hall',
+        'margaret m. and charles h. dyson center',
+        'fern tor',
+        'fontaine hall',
+        'foy townhouses',
+        'fulton street townhouses',
+        'lower fulton townhouses',
+        'gartland appartments',
+        'greystone hall',
+        'hancock center',
+        'kieran gatehouse',
+        'kirk house',
+        'leo hall',
+        'longview park',
+        'lowell thomas communications center',
+        'marian hall',
+        'marist boathouse',
+        'james j. mccann recreational center',
+        'mid-rise hall',
+        'st. anns hermitage', #st. ann's hermitage
+        'st. peters', #st. peter's
+        'sheahan hall',
+        'steel plant art studios and gallery',
+        'student center/rotunda',
+        'tennis pavilion',
+        'tenney stadium',
+        'lower townhouses',
+        'lower west cedar townhouses',
+        'upper west cedar townhouses'
+    ) ;
+    #in array
+    if(in_array($location, $campusLocations)) {
+        return true;
+    }
+    #not in array
+    else{ return false; }
 }
 
 #checks if argument is a valid date format
 function validateDate($date){
-    return false;
+    #check if the array can be split
+    if(preg_split('-', $date) == true){
+        #split the string into array
+        $dateArray = array(preg_split('-',$date));
+        if(checkdate($dateArray[0], $dateArray[1], $dateArray[2])){
+            return true;
+        }
+        else {return false ;}
+    }
+    else {
+        return false;
+    }
 }
 
 #if string is alphanumeric and if it's under desired number of characters
 function validateString($string, $length){
-    return false;
+    $string = trim($string);
+    #check if alphanumeric
+    if((ctype_alnum($string) == true) && (count($string) <= $length )) {
+        return true;
+    }
+    else {return false;}
 }
 
 function validateCategory($category){
-    return false;
+    $itemCategories = array("phone or computer", "audio or headphones", "clothing", "notebook or books", "bag or backpack", "other") ;
+    if(in_array($category, $itemCategories)) {
+        return true;
+    }
+    else {return false;}
 }
 
 function validateMonetary($amount, $limit) {
-    trim($amount);
-    return false;
+    $amount = trim($amount);
+    if((is_numeric($amount)) && ($amount >= 0) && ($amount <= $limit)){
+        return true;
+    }
+    else{
+        return false;
+    }
+
 }
 
 
